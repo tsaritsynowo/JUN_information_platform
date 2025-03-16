94a9f9aad04e3f71c4a5b4e9e9879ee2700d0175
tsaritsyn <milknnhnx@gmailcom>
2025-03-16 13:40:54 +0800

    比赛卡片标签

--- app/main/news/newsindex.jsx
+++ app/main/news/newsindex.jsx
@@ -1,24 +1,28 @@
 import { useState } from 'react';
 import competitionlist from '../../../public/file/competitionlist.json';
 
+// 以下是侧栏标签组件
+
 function Lable() {
   return (
     <div className="label">标签</div>
   );
 }
 
+// 以下是主体比赛卡片组件
+
 function Competition({id, name, description, start_date, end_date, level, labeles, content, date, exist}) {
   function openarticle({target}){
     window.open(`${window.location.href}/${target.id}`,'_blank')
   };
-  
+
   if (labeles === undefined) {
     labeles = [];
   } 
 
   const labelesHTML = labeles.map((label, index) => {
     return (
-      <div key={index} className="label">{label}</div>
+      <div key={index} className="labelstyle2">{label}</div>
     );
   });
 
@@ -27,7 +31,7 @@ function Competition({id, name, description, start_date, end_date, level, labele
       <>
         <div className="competition" id={id} onClick={openarticle}>
           <h2 id={id}>{name}</h2>
-          <samp id={id}>{start_date}至{end_date}</samp>
+          <samp id={id} className='labelstyle2'>{start_date}至{end_date}</samp>
           {labelesHTML}
           <p id={id}>{description}</p>
         </div>
@@ -37,6 +41,8 @@ function Competition({id, name, description, start_date, end_date, level, labele
     } else {
       return (<></>); };
   };
+
+  // 以下是侧栏导航框
 
 function AsideNav() {
   const [status, setStatus] = useState("competition");
@@ -53,6 +59,8 @@ function AsideNav() {
       );
 }
 
+// 以下是侧栏搜索框
+
 function Search() {
   return (
     <div className='search'>
@@ -61,6 +69,8 @@ function Search() {
     </div>
   );
   }
+
+  // 以下是侧栏标签筛选框
 
 function Fliter() {
   return (
@@ -73,6 +83,8 @@ function Fliter() {
   );
   }
 
+  // 以下是侧栏总体
+
 function Aside() {
   return (
     <aside className="aside">
@@ -84,6 +96,8 @@ function Aside() {
     </aside>
     );
 }
+
+// 以下是页面主体，包括侧栏与正栏
 
 export default function Container() {
   return (
